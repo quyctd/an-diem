1375d1435b244114c061f8325f175d4af9862071
quyctd <quy.dc98@gmail.com>
2026-06-29 00:54:20 +0700

    feat(debug): seed in-memory store + skip splash when PHORM_SEED set

--- andiem-ios/Phorm/PhormApp.swift
+++ andiem-ios/Phorm/PhormApp.swift
@@ -6,10 +6,27 @@ struct PhormApp: App {
     let container: ModelContainer
 
     @State private var pendingImport: SessionDTO?
+    #if DEBUG
+    @State private var showSplash = !ScreenshotSupport.seedRequested
+    #else
     @State private var showSplash = true
+    #endif
 
     init() {
         let schema = Schema([Session.self, Round.self, PlayerScore.self])
+        #if DEBUG
+        if ScreenshotSupport.seedRequested {
+            let config = ModelConfiguration(schema: schema, isStoredInMemoryOnly: true, cloudKitDatabase: .none)
+            do {
+                let c = try ModelContainer(for: schema, configurations: [config])
+                ScreenshotSupport.seed(into: c.mainContext, activeSession: !ScreenshotSupport.wantsEmptyHome)
+                container = c
+                return
+            } catch {
+                fatalError("Screenshot seed container failed: \(error)")
+            }
+        }
+        #endif
         let config = ModelConfiguration(
             schema: schema,
             isStoredInMemoryOnly: false,
